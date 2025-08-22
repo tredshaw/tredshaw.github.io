4639a37760faeead266ae0e37ac76ebf120dc01a
TRedshaw <toby@redshaw.me>
2025-08-22 18:01:40 +0100

    Fixed about V7

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
   base: "/",
   integrations: [tailwind(), mdx(), sitemap(), icon()],
   build: {
-    assetsPrefix: './',
+    assetsPrefix: '',
   },
   // vite: {
   //   plugins: [tailwindcss()],
