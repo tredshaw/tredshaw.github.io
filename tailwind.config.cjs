0443337420057b493dc8a9909d61ac58a5cf841e
TRedshaw <toby@redshaw.me>
2025-08-22 12:00:13 +0100

    Updated home style.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,25 +6,43 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: '#033e57',     // Dark Teal
+        primary: '#105F80',     // Dark Teal
+        primary_dark: '#07425B',     // Dark Teal
+
         secondary: '#30e6f8',   // Light Aqua
+        secondary_dark: '#23c2d2',   // Light Aqua
+
         accent: '#ff6b6b',      // Coral
+
         base: '#F9FAFB',        // Off-white base
+        base_dark: '#E2E2E3',   // Darker grey base
+
         text: '#1F2937',        // Dark slate text
-        primary_dark: '#033042',     // Dark Teal
+
+        eddy_green : '#00FFB1',  // The green accent colour
+        eddy_purple: '#B271F5',
       },
 
       backgroundColor: {
-        primary: '#033e57',     // Dark Teal
+        primary: '#105F80',     // Dark Teal
+        primary_dark: '#07425B',     // Dark Teal
+
         secondary: '#30e6f8',   // Light Aqua
         accent: '#ff6b6b',      // Coral
+
         base: '#F9FAFB',        // Off-white base
+        base_dark: '#E2E2E3',   // Darker grey base
+
         text: '#1F2937',        // Dark slate text
+
+        eddy_green : '#00FFB1',  // The green accent colour
+        eddy_purple: '#B271F5',
       },
 
       fontFamily: {
         sans: ['Inter', 'sans-serif'],
-        heading: ['Circular Std', 'sans-serif'],
+        heading: ['Inter', 'sans-serif'],
+        // heading: ['Circular Std', 'sans-serif'],
       },
     },
   },
